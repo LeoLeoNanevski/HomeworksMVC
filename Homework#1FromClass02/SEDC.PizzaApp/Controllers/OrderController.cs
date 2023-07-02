4c657554120f8d530992ed9ec12673edf0756418
LeoLeoNanevski <leonanevski87@gmail.com>
2023-07-02 16:27:05 +0200

    Add project files.

---
+++ Homework#1FromClass02/SEDC.PizzaApp/Controllers/OrderController.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SEDC.PizzaApp.Models;
+
+namespace SEDC.PizzaApp.Controllers
+{
+    public class OrderController : Controller
+    {
+        [Route("ListOrders")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        [Route("Order/Details/{id?}")]
+        public IActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new EmptyResult();
+            }
+
+            var order = _orders.FirstOrDefault(o => o.Id == id);
+            if (order == null)
+            {
+                return View("NotFound");
+            }
+
+            return View(order);
+        }
+
+        public IActionResult OrderData()
+        {
+            var order = new { Id = 12, IsDelivered = false }; // Dummy Order
+            return new JsonResult(order); // return type: JsonResult
+        }
+
+        [Route("Order/JsonData")]
+        public IActionResult JsonData()
+        {
+            var ordersData = new OrdersData
+            {
+                Id = 5,
+                NameSurname = "John Krokodilov",
+                TotalPrice = 399
+            };
+
+            // Return the model as JSON
+            return new JsonResult(ordersData);
+        }
+
+        private static List<OrdersData> _orders = new List<OrdersData>
+        {
+                new OrdersData { Id = 6, NameSurname = "Suzana Turundzieva", TotalPrice = 699 },
+                new OrdersData { Id = 7, NameSurname = "Gjoko SoModrotoOko", TotalPrice = 499 },
+        };
+
+
+
+    }
+
+}
---
+++ Homework#1FromClass02/SEDC.PizzaApp/Models/OrdersData.cs
@@ -0,0 +1,9 @@
+﻿namespace SEDC.PizzaApp.Models
+{
+    public class OrdersData
+    {
+        public int Id { get; set; }
+        public string NameSurname { get; set; } = string.Empty;
+        public int TotalPrice { get; set; }
+    }
+}
