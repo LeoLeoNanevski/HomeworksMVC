2aa802e909b74f84ec547051225c6a96b3a25505
LeoLeoNanevski <leonanevski87@gmail.com>
2023-07-03 22:39:34 +0200

    commit

---
+++ Homework#3FromClass04/SEDC.PizzaApp/ViewModels/PizzaViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace SEDC.PizzaApp.ViewModels
+{
+    public class PizzaViewModel
+    {
+        public bool IsOnPromotion { get; set; }
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Price { get; set; }
+
+    }
+}
